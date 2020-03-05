b95fdbc24ff469982a12ba92c5ee29ca2c8d0839
Igor Fomin <bytecat@mail.ru>
2020-03-05 10:00:18 +1000

    Add summary for IMaterialComponentOwner and MaterialComponent

--- Lime/Source/Widgets/Materials/MaterialComponent.cs
+++ Lime/Source/Widgets/Materials/MaterialComponent.cs
@@ -1,8 +1,17 @@
 namespace Lime
 {
+	/// <summary>
+	/// This interface must implements every node which can be used as a owner of MaterialComponent.
+	/// </summary>
 	public interface IMaterialComponentOwner
 	{
+		/// <summary>
+		/// Assign provided material
+		/// </summary>
 		void AssignMaterial(IMaterial material);
+		/// <summary>
+		/// Reset material to default
+		/// </summary>
 		void ResetMaterial();
 		ITexture Texture { get; }
 		Vector2 UV0 { get; }
@@ -16,6 +25,9 @@ namespace Lime
 
 	}
 
+	/// <summary>
+	/// Replace owner material with specified material
+	/// </summary>
 	public class MaterialComponent<T> : MaterialComponent where T : IMaterial, new()
 	{
 		protected T CustomMaterial { get; private set; }
