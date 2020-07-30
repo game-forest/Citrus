b3084c73eaf0fa6eecd73a81a28031d2a3b5cea7
Dmitry Terekhov <keltarhelviett@gmail.com>
2020-07-30 14:54:05 +1000

    [Tangerine][Animesh] Use text instead of icons in AnimeshContextualPanel

--- Tangerine/Tangerine.UI.AnimeshEditor/AnimeshContextualPanel.cs
+++ Tangerine/Tangerine.UI.AnimeshEditor/AnimeshContextualPanel.cs
@@ -12,20 +12,20 @@ namespace Tangerine.UI.AnimeshEditor
 			RootNode = new Widget {
 				MinMaxSize = new Vector2(100f, 50f),
 				Nodes = {
-					CreateAnimeshStateButton(AnimeshTools.ModificationState.Modification, (Command)Tools.AnimeshModify),
-					CreateAnimeshStateButton(AnimeshTools.ModificationState.Creation, (Command)Tools.AnimeshCreate),
-					CreateAnimeshStateButton(AnimeshTools.ModificationState.Removal, (Command)Tools.AnimeshRemove),
-					CreateAnimeshStateButton(AnimeshTools.ModificationState.Animation, (Command)Tools.AnimeshAnimate),
+					CreateAnimeshStateButton(AnimeshTools.ModificationState.Modification, (Command)Tools.AnimeshModify, "Modify"),
+					CreateAnimeshStateButton(AnimeshTools.ModificationState.Creation, (Command)Tools.AnimeshCreate, "Create"),
+					CreateAnimeshStateButton(AnimeshTools.ModificationState.Removal, (Command)Tools.AnimeshRemove, "Remove"),
+					CreateAnimeshStateButton(AnimeshTools.ModificationState.Animation, (Command)Tools.AnimeshAnimate, "Animate"),
 				},
 				Layout = new TableLayout { RowCount = 1, ColumnCount = 4, },
 				LayoutCell = new LayoutCell(new Alignment { X = HAlignment.Center, Y = VAlignment.Bottom, }),
-				CompoundPresenter = { new WidgetFlatFillPresenter(Theme.Colors.GrayBackground), new WidgetBoundsPresenter(Color4.Red)},
+				CompoundPresenter = { new WidgetFlatFillPresenter(Theme.Colors.GrayBackground), new WidgetBoundsPresenter(Color4.Red) },
 			};
 		}
 
-		private static Widget CreateAnimeshStateButton(AnimeshTools.ModificationState state, Command command)
+		private static Widget CreateAnimeshStateButton(AnimeshTools.ModificationState state, Command command, string text)
 		{
-			var tb = new ToolbarButton(command.Icon.AsTexture) {
+			var tb = new ToolbarButton(text) {
 				Highlightable = true,
 				Tooltip = command.Text,
 			};
