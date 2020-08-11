f4cf01d522eb6878a70b643e41c84017419458b4
Dmitry Shcherbina <35944960+Nitrionis@users.noreply.github.com>
2020-08-11 20:50:22 +1000

    [CIT-844][Overdraw][Profiler][Lime][Win][Android][Mac][iOS] Add SceneProfilingInfo class.

---
+++ Lime/Source/Profiler/SceneProfilingInfo.cs
@@ -0,0 +1,12 @@
+#if PROFILER
+namespace Lime.Profiler
+{
+	public static class SceneProfilingInfo
+	{
+		/// <summary>
+		/// Node manager for scene objects.
+		/// </summary>
+		public static object NodeManager;
+	}
+}
+#endif // PROFILER
