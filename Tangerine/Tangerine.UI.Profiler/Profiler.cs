68a699eb963d38c3537319aa2ada14b427f231f1
Dmitry Shcherbina <35944960+Nitrionis@users.noreply.github.com>
2020-09-12 15:00:18 +1000

    [CIT-844][Overdraw][Profiler][Tangerine][Win][Mac] Add Profiler panel.

---
+++ Tangerine/Tangerine.UI.Profiler/Profiler.cs
@@ -0,0 +1,29 @@
+using System;
+using Lime;
+using Tangerine.UI.Docking;
+
+namespace Tangerine.UI
+{
+	public class Profiler
+	{
+		public static Profiler Instance { get; private set; }
+
+		private readonly Panel panel;
+		public readonly Widget RootWidget;
+
+		public Profiler(Panel panel)
+		{
+			if (Instance != null) {
+				throw new InvalidOperationException();
+			}
+			Instance = this;
+			this.panel = panel;
+			RootWidget = new Widget {
+				Layout = new VBoxLayout {
+					Spacing = 6
+				}
+			};
+			panel.ContentWidget.AddNode(RootWidget);
+		}
+	}
+}
