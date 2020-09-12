1982e08a12ab468fb78c08dad29d9a8c587c5e86
Dmitry Shcherbina <35944960+Nitrionis@users.noreply.github.com>
2020-09-12 15:23:28 +1000

    [CIT-844][Overdraw][Profiler][Tangerine][Win][Mac] Add OverdrawController.

---
+++ Tangerine/Tangerine.UI.Profiler/OverdrawController.cs
@@ -0,0 +1,13 @@
+using Lime;
+using Lime.Profiler.Graphics;
+
+namespace Tangerine.UI
+{
+	internal class OverdrawController : Widget
+	{
+		public OverdrawController()
+		{
+			
+		}
+	}
+}
--- Tangerine/Tangerine.UI.Profiler/Profiler.cs
+++ Tangerine/Tangerine.UI.Profiler/Profiler.cs
@@ -24,6 +24,7 @@ namespace Tangerine.UI
 				}
 			};
 			panel.ContentWidget.AddNode(RootWidget);
+			RootWidget.AddNode(new OverdrawController());
 		}
 	}
 }
