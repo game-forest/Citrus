3e01cec217105d48c58414eefad28a5c3330bb3d
Konstantin Uzymov <konstantinreed@gmail.com>
2020-09-30 15:47:07 +1000

    [CIT-1141][Tangerine] Remove unnecessary code

--- Tangerine/Tangerine/Dialogs/Lookup/LookupDialog.cs
+++ Tangerine/Tangerine/Dialogs/Lookup/LookupDialog.cs
@@ -66,60 +66,6 @@ namespace Tangerine
 			lookupWidget.Canceled += LookupCanceled;
 			windowWidget.FocusScope.SetDefaultFocus();
 			sections.Initialize(sectionType);
-
-			// TODO: Adaptive window height. Fix problem with "window.Resized += deviceRotated => UpdateAndResize(0);" at DefaultWindowWidget class.
-			//var layoutProcessor = windowWidget.Manager.Processors.OfType<LayoutProcessor>().First();
-			//layoutProcessor.Update(0);
-			//var minHeight = 0f;
-			//var vSpacing = ((VBoxLayout)lookupWidget.Layout).Spacing;
-			//minHeight += windowWidget.Padding.Top + windowWidget.Padding.Bottom;
-			//minHeight += lookupWidget.Padding.Top + lookupWidget.Padding.Bottom;
-			//var isFirstWidget = true;
-			//foreach (var widget in lookupWidget.Nodes.OfType<Widget>()) {
-			//	if (widget.Visible) {
-			//		if (widget != lookupWidget.ScrollView) {
-			//			if (!isFirstWidget) {
-			//				minHeight += vSpacing;
-			//			}
-			//			minHeight += widget.Height;
-			//		}
-			//		isFirstWidget = false;
-			//	}
-			//}
-
-			//var previousLookupItemsCount = int.MinValue;
-			//var previousLookupItemHeight = float.MinValue;
-			//windowWidget.Components.GetOrAdd<PostLateUpdateBehavior>().Updating += () => {
-			//	var lookupItemsCount = lookupWidget.ScrollView.Content.Nodes.Count;
-			//	var lookupItemHeight = lookupItemsCount > 0 ? lookupWidget.ScrollView.Content.Nodes.OfType<Widget>().First().Height : 0;
-			//	if (lookupItemsCount != previousLookupItemsCount || lookupItemHeight != previousLookupItemHeight) {
-			//		layoutProcessor.Update(0);
-
-			//		lookupItemHeight = lookupItemsCount > 0 ? lookupWidget.ScrollView.Content.Nodes.OfType<Widget>().First().Height : 0;
-			//		previousLookupItemsCount = lookupItemsCount;
-			//		previousLookupItemHeight = lookupItemHeight;
-
-			//		var scrollViewHeight = 0f;
-			//		if (lookupItemsCount > 0) {
-			//			scrollViewHeight += vSpacing;
-			//			scrollViewHeight += Math.Min(lookupItemsCount, 10) * lookupItemHeight;
-			//		}
-
-			//		windowWidget.Height = minHeight + scrollViewHeight;
-			//		window.ClientSize = new Vector2(window.ClientSize.X, minHeight + scrollViewHeight);
-
-			//		System.Console.WriteLine($"Scroll height: {scrollViewHeight:F0}; Window height: {minHeight + scrollViewHeight:F0}");
-			//	}
-			//};
 		}
-
-		//[NodeComponentDontSerialize]
-		//[UpdateStage(typeof(PostLateUpdateStage))]
-		//private class PostLateUpdateBehavior : BehaviorComponent
-		//{
-		//	public event Action Updating;
-
-		//	protected override void Update(float delta) => Updating?.Invoke();
-		//}
 	}
 }
