8b3cb5cc4d328b4461f10838d85c0e5338271f86
Denis Rotanov <mapholameth@gmail.com>
2021-02-02 15:00:07 +1000

    [CIT-1830] handle trailing '/' in tang. menu path attr; add comments

--- Tangerine/Tangerine.Core/MenuExtensions.cs
+++ Tangerine/Tangerine.Core/MenuExtensions.cs
@@ -6,11 +6,18 @@ namespace Tangerine.Core
 {
 	public static class MenuExtensions
 	{
+		/// <summary>
+		/// Inserts command into menu, creating intermediate nested menus as specified by path.
+		/// </summary>
+		/// <param name="menu">Target menu.</param>
+		/// <param name="command">Command to insert.</param>
+		/// <param name="path"><see cref="Lime.TangerineMenuPathAttribute"/></param>
 		public static void InsertCommandAlongPath(this IMenu menu, ICommand command, string path)
 		{
 			IMenu nextMenu = menu;
+			bool endsWithSlash = path.EndsWith("/");
 			var parts = path.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
-			for (int i = 0; i < parts.Length - 1; i++) {
+			for (int i = 0; i < parts.Length - (endsWithSlash ? 0 : 1); i++) {
 				ICommand nestedMenuCommand = nextMenu.FirstOrDefault(c => c.Text == parts[i]);
 				if (nestedMenuCommand == null) {
 					nestedMenuCommand = new Command(parts[i], new Menu());
@@ -18,7 +25,9 @@ namespace Tangerine.Core
 				}
 				nextMenu = nestedMenuCommand.Menu;
 			}
-			command.Text = parts[parts.Length - 1];
+			if (!endsWithSlash) {
+				command.Text = parts[parts.Length - 1];
+			}
 			nextMenu.Add(command);
 		}
 	}
