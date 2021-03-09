a73a0a22422fbaa7a74f1a1c826ae6a07e15a095
Denis Rotanov <mapholameth@gmail.com>
2021-03-09 12:58:45 +1000

    [CIT-1383] CustomSetAssetBundle: throw if file doesn't exist in provided list

--- Lime/Source/AssetBundle/CustomSetAssetBundle.cs
+++ Lime/Source/AssetBundle/CustomSetAssetBundle.cs
@@ -1,21 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 
 namespace Lime
 {
 	/// <summary>
 	/// Asset bundle which substitutes enumeration results with the given list of files.
+	/// All file operations will throw FileNotFoundException if provided file path
+	/// does not exist in provided list of files.
 	/// </summary>
 	public class CustomSetAssetBundle : WrappedAssetBundle
 	{
 		private readonly List<string> files;
+		private readonly HashSet<string> fileSet;
 
 		public CustomSetAssetBundle(AssetBundle bundle, IEnumerable<string> files)
 			: base (bundle)
 		{
 			this.files = files.ToList();
+			fileSet = new HashSet<string>(files);
 		}
 
 		public override IEnumerable<string> EnumerateFiles(string path = null, string extension = null)
@@ -29,6 +34,72 @@ namespace Lime
 				}
 				yield return file;
 			}
+		}
+
+		public override bool FileExists(string path) => fileSet.Contains(AssetPath.CorrectSlashes(path));
+
+		public override Stream OpenFile(string path, FileMode fileMode = FileMode.Open)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			return base.OpenFile(path, fileMode);
+		}
+
+		public override Stream OpenFileRaw(string path, FileMode fileMode = FileMode.Open)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			return base.OpenFileRaw(path, fileMode);
+		}
+
+		public override void DeleteFile(string path)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			base.DeleteFile(path);
+		}
+
+		public override int GetFileSize(string path)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			return base.GetFileSize(path);
+		}
+
+		public override int GetFileUnpackedSize(string path)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			return base.GetFileUnpackedSize(path);
+		}
+
+		public override AssetAttributes GetFileAttributes(string path)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			return base.GetFileAttributes(path);
+		}
+
+		public override SHA256 GetFileCookingUnitHash(string path)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			return base.GetFileCookingUnitHash(path);
+		}
+
+		public override SHA256 GetFileContentsHash(string path)
+		{
+			if (!FileExists(path)) {
+				throw new FileNotFoundException(null, path);
+			}
+			return base.GetFileContentsHash(path);
 		}
 	}
 }
