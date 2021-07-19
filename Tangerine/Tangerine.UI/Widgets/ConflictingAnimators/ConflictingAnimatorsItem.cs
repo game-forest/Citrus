51aadb5389e5f2b0683dc2172bd300cccdd287cc
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2021-07-19 20:34:19 +1000

    [CIT-2027] Fix navigation

--- Tangerine/Tangerine.UI/Widgets/ConflictingAnimators/ConflictingAnimatorsItem.cs
+++ Tangerine/Tangerine.UI/Widgets/ConflictingAnimators/ConflictingAnimatorsItem.cs
@@ -1,6 +1,7 @@
 using Lime;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Tangerine.Core;
 using Tangerine.Core.Operations;
 
@@ -8,14 +9,16 @@ namespace Tangerine.UI
 {
 	public class ConflictingAnimatorsItem : Frame
 	{
+		private Node cachedNode;
+
 		protected const float IconSize = 16;
 		protected const float IconRightPadding = 5;
 
-		protected readonly ConflictingAnimatorsInfo info;
+		public readonly ConflictingAnimatorsInfo Info;
 
 		public ConflictingAnimatorsItem(ConflictingAnimatorsInfo info)
 		{
-			this.info = info;
+			Info = info;
 
 			Layout = new VBoxLayout { Spacing = 4 };
 			foreach (var widget in CreateContent()) {
@@ -26,8 +29,15 @@ namespace Tangerine.UI
 			Gestures.Add(new DoubleClickGesture(() => {
 				try {
 					Project.Current.OpenDocument(info.DocumentPath);
-					NavigateToNode.Perform(info.Node, enterInto: false, turnOnInspectRootNodeIfNeeded: true);
+					if (cachedNode == null) {
+						cachedNode = Document.Current.RootNode.FindNode(info.RelativePath);
+						if (info.IndexForPathCollisions.HasValue) {
+							cachedNode = cachedNode.Parent.Nodes[info.IndexForPathCollisions.Value];
+						}
+					}
+					NavigateToNode.Perform(cachedNode, enterInto: false, turnOnInspectRootNodeIfNeeded: true);
 				} catch (System.Exception e) {
+					new AlertDialog($"Couldn't navigate to node {info.RelativePath} in {info.DocumentPath}");
 					Logger.Write(e.Message);
 				}
 			}));
@@ -39,30 +49,29 @@ namespace Tangerine.UI
 		protected IEnumerable<Widget> CreateContent()
 		{
 			// Display affected scene.
-			if (!string.IsNullOrEmpty(info.DocumentPath)) {
-				var sceneTexture = IconPool.GetIcon("Lookup.SceneFileIcon").AsTexture;
-				yield return new Widget {
-					Layout = new HBoxLayout(),
-					Padding = new Thickness(left: 5),
-					Nodes = {
-						new Image {
-							LayoutCell = new LayoutCell {
-								Stretch = Vector2.Zero,
-								Alignment = new Alignment { X = HAlignment.Center, Y = VAlignment.Center }
-							},
-							Padding = new Thickness(right: IconRightPadding),
-							MinMaxSize = new Vector2(IconSize + IconRightPadding, IconSize),
-							Texture = sceneTexture,
+			var sceneTexture = IconPool.GetIcon("Lookup.SceneFileIcon").AsTexture;
+			yield return new Widget {
+				Layout = new HBoxLayout(),
+				Padding = new Thickness(left: 5),
+				Nodes = {
+					new Image {
+						LayoutCell = new LayoutCell {
+							Stretch = Vector2.Zero,
+							Alignment = new Alignment { X = HAlignment.Center, Y = VAlignment.Center }
 						},
-						new ThemedSimpleText {
-							Text = info.DocumentPath,
-						},
+						Padding = new Thickness(right: IconRightPadding),
+						MinMaxSize = new Vector2(IconSize + IconRightPadding, IconSize),
+						Texture = sceneTexture,
 					},
-				};
-			}
+					new ThemedSimpleText {
+						Text = Info.DocumentPath,
+					},
+				},
+			};
 
 			// Display affected widget.
-			var iconTexture = NodeIconPool.GetTexture(info.Node);
+			var iconTexture = NodeIconPool.GetTexture(Info.NodeType);
+			var relativePathVerbose = string.Join(" in ", Info.RelativePath.Split('/').Reverse().Select(i => $"'{i}'"));
 			yield return new Widget {
 				Layout = new HBoxLayout(),
 				Padding = new Thickness(left: 5),
@@ -77,7 +86,7 @@ namespace Tangerine.UI
 						Texture = iconTexture,
 					},
 					new ThemedSimpleText {
-						Text = info.Node.ToString(),
+						Text = relativePathVerbose,
 					},
 				},
 			};
@@ -87,14 +96,14 @@ namespace Tangerine.UI
 				Layout = new VBoxLayout { Spacing = 2 },
 				Padding = new Thickness(left: 5),
 			};
-			for (var i = 0; i < info.AffectedProperties.Length; ++i) {
+			for (var i = 0; i < Info.AffectedProperties.Length; ++i) {
 				conflicts.AddNode(new Widget {
 					Layout = new HBoxLayout(),
 					Padding = new Thickness(left: 5),
 					Nodes = {
 						Spacer.HSpacer(IconSize + IconRightPadding),
 						new ThemedSimpleText {
-							Text = $"{info.AffectedProperties[i]}: {string.Join(", ", info.ConcurrentAnimations[i])}",
+							Text = $"Target Property: {Info.AffectedProperties[i]}; Potential Conflicts: {string.Join(", ", Info.ConcurrentAnimations[i])}",
 						},
 					},
 				});
@@ -154,25 +163,36 @@ namespace Tangerine.UI
 
 	public class ConflictingAnimatorsInfo : IEquatable<ConflictingAnimatorsInfo>
 	{
-		public readonly Node Node;
+		public readonly Type NodeType;
+		public readonly string RelativePath;
 		public readonly string DocumentPath;
 		public readonly string[] AffectedProperties;
-		public readonly HashSet<string>[] ConcurrentAnimations;
+		public readonly SortedSet<string>[] ConcurrentAnimations;
+		public readonly int? IndexForPathCollisions;
 
-		public ConflictingAnimatorsInfo(Node node, string documentPath, string[] affectedProperties, HashSet<string>[] concurrentAnimations)
-		{
-			Node = node;
+		public ConflictingAnimatorsInfo(
+			Type nodeType,
+			string relativePath,
+			string documentPath,
+			string[] affectedProperties,
+			SortedSet<string>[] concurrentAnimations,
+			int? indexForPathCollisions
+		) {
+			NodeType = nodeType;
+			RelativePath = relativePath;
 			DocumentPath = documentPath;
 			AffectedProperties = affectedProperties;
 			ConcurrentAnimations = concurrentAnimations;
+			IndexForPathCollisions = indexForPathCollisions;
 		}
 
 		public bool Equals(ConflictingAnimatorsInfo other) =>
 			ReferenceEquals(this, other) ||
+			RelativePath == other.RelativePath &&
 			DocumentPath == other.DocumentPath &&
-			Node.GetRelativePath() == other.Node.GetRelativePath();
+			IndexForPathCollisions == other.IndexForPathCollisions;
 
 		public override bool Equals(object obj) => Equals(obj as ConflictingAnimatorsInfo);
-		public override int GetHashCode() => HashCode.Combine(Node, DocumentPath, AffectedProperties, ConcurrentAnimations);
+		public override int GetHashCode() => HashCode.Combine(RelativePath, DocumentPath, AffectedProperties, ConcurrentAnimations);
 	}
 }
--- Tangerine/Tangerine/Dialogs/ConflictingAnimators/ConflictingAnimatorsInfoProvider.cs
+++ Tangerine/Tangerine/Dialogs/ConflictingAnimators/ConflictingAnimatorsInfoProvider.cs
@@ -14,50 +14,56 @@ namespace Tangerine
 		{
 			if (Project.Current == null) yield break;
 
+			assetName ??= string.Empty;
 			var used = new HashSet<string>();
 			var assets = Project.Current.AssetDatabase
-				.Where(asset => asset.Value.Type == AssetType && asset.Value.Path.EndsWith(assetName ?? string.Empty))
+				.Where(asset => asset.Value.Type == AssetType && asset.Value.Path.EndsWith(assetName))
 				.Select(asset => asset.Value.Path);
+
 			foreach (var asset in assets) {
 				if (used.Contains(asset)) continue;
-
 				used.Add(asset);
+
 				var root = Node.Load(asset);
-				var queue = new Queue<(Node node, string document)>();
-				foreach (var node in root.Nodes) {
-					queue.Enqueue((node, asset));
-				}
+				var queue = new Queue<Node>(root.Nodes);
 				while (queue.Count > 0) {
-					var (node, document) = queue.Dequeue();
-					var properties = new Dictionary<string, HashSet<string>>();
-					foreach (var animator in node.Animators) {
-						var id = animator.AnimationId;
-						var property = animator.TargetPropertyPath;
-						if (!properties.TryGetValue(animator.TargetPropertyPath, out var hash)) {
-							properties[property] = new HashSet<string>();
+					var node = queue.Dequeue();
+					var conflicts = node.Animators
+						.GroupBy(i => i.TargetPropertyPath)
+						.Where(i => i.Count() > 1)
+						.Select(i => (property: i.Key, animations: new SortedSet<string>(i.Select(j => j.AnimationId ?? "Legacy"))));
+
+					if (conflicts.Any()) {
+						var documentPath = asset;
+						var relativePathStack = new Stack<string>();
+						relativePathStack.Push(node.Id);
+						foreach (var ancestor in node.Ancestors) {
+							if (ancestor.Components.TryGet<Node.AssetBundlePathComponent>(out var pathComponent)) {
+								documentPath = pathComponent.Path.Replace(AssetType, string.Empty);
+								break;
+							}
+							relativePathStack.Push(ancestor.Id);
 						}
-						properties[property].Add(id ?? "Legacy");
-					}
-					var conflictingProperties = new List<string>();
-					var conflictingAnimations = new List<HashSet<string>>();
-					foreach (var (property, animations) in properties) {
-						if (animations.Count > 1) {
-							conflictingProperties.Add(property);
-							conflictingAnimations.Add(animations);
-						}
-					}
-					if (conflictingProperties.Any()) {
+						var relativePath = string.Join("/", relativePathStack);
 						yield return new ConflictingAnimatorsInfo(
-							node,
-							document,
-							conflictingProperties.ToArray(),
-							conflictingAnimations.ToArray()
+							node.GetType(),
+							relativePath,
+							documentPath,
+							conflicts.Select(i => i.property).ToArray(),
+							conflicts.Select(i => i.animations).ToArray(),
+							indexForPathCollisions: node.Parent?.Nodes.IndexOf(node)
 						);
 					}
-					used.Add(node.ContentsPath);
+
 					var isExternal = !string.IsNullOrEmpty(node.ContentsPath);
-					foreach (var child in node.Nodes) {
-						queue.Enqueue((child, isExternal ? node.ContentsPath : document));
+					if (isExternal) {
+						foreach (var child in Get(node.ContentsPath)) {
+							yield return child;
+						}
+					} else {
+						foreach (var child in node.Nodes) {
+							queue.Enqueue(child);
+						}
 					}
 				}
 			}
