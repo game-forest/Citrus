a113520e5a9066fa5ed13a248f01aa82b037ee29
Nikita Baranov <nbaranov@game-forest.com>
2021-08-02 18:00:46 +1000

    [CIT-2087][Tangerine] Add information in About window

--- Tangerine/Tangerine.UI/About.cs
+++ Tangerine/Tangerine.UI/About.cs
@@ -1,4 +1,8 @@
 using Lime;
+using Orange;
+using System.IO;
+using System.Text;
+using Tangerine.Core;
 
 namespace Tangerine.UI
 {
@@ -9,7 +13,21 @@ namespace Tangerine.UI
 	{
 		public static void DisplayInformation()
 		{
-			string info = "Hello";
+			var projectDir = Path.GetDirectoryName(Project.Current?.CitprojPath ?? "");
+			StringBuilder stringbBuilder = new StringBuilder();
+			var citrusDir = Orange.Toolbox.FindCitrusDirectory();
+			Git.Update(projectDir);
+			Git.Exec(projectDir, "remote -v", stringbBuilder);
+			var projectRemoteUri = stringbBuilder.ToString().IsNullOrWhiteSpace() ?
+				"" : stringbBuilder.ToString().Split('\n')[0].Split(' ', '\t')[1];
+			Git.Exec(projectDir, "rev-parse HEAD", stringbBuilder.Clear());
+			var projectLatestCommit = stringbBuilder.ToString();
+			Git.Update(citrusDir);
+			Git.Exec(citrusDir, "rev-parse HEAD", stringbBuilder.Clear());
+			var citrusLatestCommit = stringbBuilder.ToString();
+			string info = $"Project remote url: {projectRemoteUri} \n" +
+						  $"Project commit: {projectLatestCommit}" +
+						  $"Citrus commit: {citrusLatestCommit}";
 			var alertDialog = new AlertDialog(info, "Copy", "Close");
 			switch (alertDialog.Show()) {
 				case 0: {
