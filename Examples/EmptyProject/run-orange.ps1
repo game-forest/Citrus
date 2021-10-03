e163f393f1df81d9fb8f4ab1b779e5d69e090f63
Denis Rotanov <mapholameth@gmail.com>
2021-10-04 00:07:02 +1000

    [CIT-2154][Examples] Fix run scripts

--- Examples/EmptyProject/run-orange.ps1
+++ Examples/EmptyProject/run-orange.ps1
@@ -2,4 +2,4 @@ $CurrentPath = Get-Location
 $DotnetPath = Join-Path -Path $CurrentPath -ChildPath "dotnet"
 $Env:DOTNET_ROOT = $DotnetPath
 $Env:Path += $DotnetPath
-Invoke-Expression "&'.\..\..\Citrus\Orange\Launcher\bin\Win\Release\Launcher.exe'"
+Invoke-Expression "&'.\..\..\Orange\Launcher\bin\Win\Release\Launcher.exe'"
--- Examples/EmptyProject/run-tangerine.ps1
+++ Examples/EmptyProject/run-tangerine.ps1
@@ -2,4 +2,4 @@ $CurrentPath = Get-Location
 $DotnetPath = Join-Path -Path $CurrentPath -ChildPath "dotnet"
 $Env:DOTNET_ROOT = $DotnetPath
 $Env:Path += ';' + $DotnetPath
-Invoke-Expression "&'.\..\..\Citrus\Orange\Launcher\bin\Win\Release\Launcher.exe' -b Tangerine\Tangerine.Win.sln -r Tangerine\Tangerine\bin\Release\Tangerine.exe"
+Invoke-Expression "&'.\..\..\Orange\Launcher\bin\Win\Release\Launcher.exe' -b Tangerine\Tangerine.Win.sln -r Tangerine\Tangerine\bin\Release\Tangerine.exe"
