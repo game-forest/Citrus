a7c924d9fabfa15b0588be6ec1fd20bdd2a8efd5
Dmitriy Shcherbina <nitrodimon@mail.ru>
2021-10-28 09:55:59 +1000

    [CIT-2016][Lime] Make some methods in SimpleText public
    This will be needed to calculate the height of the tooltip

--- Lime/Source/Widgets/Text/SimpleText.cs
+++ Lime/Source/Widgets/Text/SimpleText.cs
@@ -452,7 +452,7 @@ namespace Lime
 			return 0;
 		}
 
-		private float CalcTotalHeight(int numLines)
+		public float CalcTotalHeight(int numLines)
 		{
 			return Math.Max(FontHeight * numLines + Spacing * (numLines - 1), FontHeight);
 		}
@@ -471,7 +471,7 @@ namespace Lime
 			}
 		}
 
-		private List<string> SplitText(string text)
+		public List<string> SplitText(string text)
 		{
 			var strings = new List<string>(text.Split('\n'));
 			// Add linebreaks to make editor happy.
@@ -510,7 +510,7 @@ namespace Lime
 			return Font.MeasureTextLine(line, FontHeight, start, count, letterSpacing + font.Spacing).X <= ContentWidth;
 		}
 
-		private Vector2 MeasureTextLine(string line)
+		public Vector2 MeasureTextLine(string line)
 		{
 			return Font.MeasureTextLine(line, FontHeight, letterSpacing + font.Spacing);
 		}
