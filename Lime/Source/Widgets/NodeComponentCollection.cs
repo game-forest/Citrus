6539a66e7a41792e355538233b3fa511bb1c0fbe
Dmitriy Shcherbina <nitrodimon@mail.ru>
2021-10-28 14:32:40 +1000

    [CIT-2016][Lime] Add ITimelineIconProvider

--- Lime/Source/Widgets/NodeComponentCollection.cs
+++ Lime/Source/Widgets/NodeComponentCollection.cs
@@ -282,4 +282,33 @@ namespace Lime
 			owner.Manager?.UnregisterComponent(component, owner);
 		}
 	}
+	
+	
+	public interface ITimelineIconProvider
+	{
+		/// <summary>
+		/// Texture version.
+		/// </summary>
+		/// <remarks>
+		/// <para>Changes of this property are checked in each frame.</para>
+		/// <para>It is guaranteed that this property will be read before the others.</para>
+		/// </remarks>
+		int TextureVersion { get; }
+		
+		/// <summary>
+		/// A small texture that will be drawn on the timeline.
+		/// </summary>
+		/// <remarks>
+		/// Changes of this property will be processed only after changing the <see cref="TextureVersion"/> property.
+		/// </remarks>
+		ITexture Texture { get; }
+		
+		/// <summary>
+		/// A tooltip that will be displayed when you hover over the icon.
+		/// </summary>
+		/// <remarks>
+		/// The tooltip will be reassigned to an icon in each frame.
+		/// </remarks>
+		string Tooltip { get; }
+	}
 }
