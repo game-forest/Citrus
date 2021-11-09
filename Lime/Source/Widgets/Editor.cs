cdfba9db3293ffa89a9e865e964978e74fc59d66
Alexey Manyakin <amanyakin@sgfwest.com>
2021-11-09 17:34:52 +1000

    [CIT-2118] Fix EnsureSelection instead of SelectAll

--- Lime/Source/Widgets/Editor.cs
+++ Lime/Source/Widgets/Editor.cs
@@ -214,7 +214,7 @@ namespace Lime
 
 		private void EnsureSelection()
 		{
-			if (SelectionStart.IsVisible) {
+			if (HasSelection()) {
 				return;
 			}
 			SelectionStart.IsVisible = SelectionEnd.IsVisible = true;
@@ -348,7 +348,7 @@ namespace Lime
 
 		public void SelectAll()
 		{
-			SelectionStart.IsVisible = SelectionEnd.IsVisible = Text.Text.Length > 0;
+			SelectionStart.IsVisible = SelectionEnd.IsVisible = true;
 			SelectionStart.TextPos = 0;
 			SelectionEnd.TextPos = int.MaxValue;
 			Window.Current.Invalidate();
