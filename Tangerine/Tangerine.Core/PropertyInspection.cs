b535d8319d780499b8c46883eb9d1b74c629e6c8
Alexey Manyakin <amanyakin@sgfwest.com>
2022-01-12 16:42:18 +1000

    [CIT-2198] Cache CanInspectProperty and CanAnimateProperty methods

--- Tangerine/Tangerine.Core/PropertyInspection.cs
+++ Tangerine/Tangerine.Core/PropertyInspection.cs
@@ -1,12 +1,38 @@
 using Lime;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Tangerine.Core
 {
 	public static class PropertyInspection
 	{
+		private static readonly Dictionary<(Type, PropertyInfo), bool> CanAnimatePropertyCache
+			= new Dictionary<(Type, PropertyInfo), bool>();
+		private static readonly Dictionary<(Type, PropertyInfo), bool> CanInspectPropertyCache
+			= new Dictionary<(Type, PropertyInfo), bool>();
+
 		public static bool CanInspectProperty(Type type, PropertyInfo property)
+		{
+			if (CanInspectPropertyCache.ContainsKey((type, property))) {
+				return CanInspectPropertyCache[(type, property)];
+			}
+			bool result = _CanInspectProperty(type, property);
+			CanInspectPropertyCache[(type, property)] = result;
+			return result;
+		}
+
+		public static bool CanAnimateProperty(Type type, PropertyInfo property)
+		{
+			if (CanAnimatePropertyCache.ContainsKey((type, property))) {
+				return CanAnimatePropertyCache[(type, property)];
+			}
+			bool result = _CanAnimateProperty(type, property);
+			CanAnimatePropertyCache[(type, property)] = result;
+			return result;
+		}
+
+		private static bool _CanInspectProperty(Type type, PropertyInfo property)
 		{
 			if (property.GetIndexParameters().Length > 0) {
 				// we don't inspect indexers (they have "Item" name by default)
@@ -30,7 +56,7 @@ namespace Tangerine.Core
 			return yuzuItem != null || hasKeyframeColor;
 		}
 
-		public static bool CanAnimateProperty(Type type, PropertyInfo property)
+		private static bool _CanAnimateProperty(Type type, PropertyInfo property)
 		{
 			bool canInspect = CanInspectProperty(type, property);
 			bool isIanimationHost = typeof(IAnimationHost).IsAssignableFrom(type);
