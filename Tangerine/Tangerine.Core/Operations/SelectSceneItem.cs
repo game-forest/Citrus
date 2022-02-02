8dcf0c02c0307294aff8aab6b9ed97670fabdbaa
Dmitriy Shcherbina <nitrodimon@mail.ru>
2022-02-02 22:41:00 +1000

    [CIT-1920][Tangerine] Add ExpandPath operation

--- Tangerine/Tangerine.Core/Operations/SelectSceneItem.cs
+++ Tangerine/Tangerine.Core/Operations/SelectSceneItem.cs
@@ -18,31 +18,40 @@ namespace Tangerine.Core.Operations
 		}
 	}
 
+	public static class ExpandPath
+	{
+		public static void Perform(Node node)
+		{
+			var sceneItem = Document.Current.GetSceneItemForObject(node);
+			for (var i = sceneItem.Parent; i != null; i = i.Parent) {
+				if (i.TryGetNode(out var n) && n == Document.Current.Container) {
+					break;
+				}
+				if (!i.GetTimelineSceneItemState().NodesExpanded) {
+					DelegateOperation.Perform(
+						Document.Current.BumpSceneTreeVersion, undo: null, isChangingDocument: false
+					);
+					SetProperty.Perform(
+						obj: i.GetTimelineSceneItemState(),
+						propertyName: nameof(TimelineSceneItemStateComponent.NodesExpanded),
+						value: true,
+						isChangingDocument: false
+					);
+					DelegateOperation.Perform(
+						null, Document.Current.BumpSceneTreeVersion, isChangingDocument: false
+					);
+				}
+			}
+		}
+	}
+
 	public static class SelectNode
 	{
 		public static void Perform(Node node, bool select = true)
 		{
 			var sceneItem = Document.Current.GetSceneItemForObject(node);
 			if (select) {
-				for (var i = sceneItem.Parent; i != null; i = i.Parent) {
-					if (i.TryGetNode(out var n) && n == Document.Current.Container) {
-						break;
-					}
-					if (!i.GetTimelineSceneItemState().NodesExpanded) {
-						DelegateOperation.Perform(
-							Document.Current.BumpSceneTreeVersion, undo: null, isChangingDocument: false
-						);
-						SetProperty.Perform(
-							obj: i.GetTimelineSceneItemState(),
-							propertyName: nameof(TimelineSceneItemStateComponent.NodesExpanded),
-							value: true,
-							isChangingDocument: false
-						);
-						DelegateOperation.Perform(
-							null, Document.Current.BumpSceneTreeVersion, isChangingDocument: false
-						);
-					}
-				}
+				ExpandPath.Perform(node);
 			}
 			SelectSceneItem.Perform(sceneItem, select);
 		}
